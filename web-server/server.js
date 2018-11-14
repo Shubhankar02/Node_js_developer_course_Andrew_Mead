4ffb2fd1c087ec6c30fccb9f1d9bf1c104e55e0f
Shubhankar02 <shborade@gmail.com>
2018-11-14 19:10:35 +0530

    add middleware

--- web-server/server.js
+++ web-server/server.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now}, Method:${req.method}, URL:${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) console.log(err);
+    })
+    next();
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintainance');
+// })
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
 app.get('/', (req, res) => {
+    console.log(req.headers);
     res.render('home', {
         pageTitle : 'Home page',
-        message : 'Welcome, this is home page',
-        currentYear : new Date().getFullYear()
+        message : 'Welcome, this is home page'
     });
 });
 
@@ -20,8 +41,7 @@ app.get('/help', (req, res) => {
 
 app.get('/about', (req,res) => {
     res.render('about', {
-        pageTitle : 'About page',
-        currentYear : new Date().getFullYear()
+        pageTitle : 'About page'
     });
 });
 
