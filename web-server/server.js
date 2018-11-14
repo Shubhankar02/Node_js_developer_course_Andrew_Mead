b0b933450ea9fce12790c4936c0427a926733b49
Shubhankar02 <shborade@gmail.com>
2018-11-14 19:58:51 +0530

    ready for deployment

--- web-server/server.js
+++ web-server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -45,6 +45,6 @@ app.get('/about', (req,res) => {
     });
 });
 
-app.listen(3000, ()=>{
-    console.log('Server has started on PORT 3000');
+app.listen(port, ()=>{
+    console.log(`Server has started on PORT ${port}`);
 });
