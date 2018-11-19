aa8b33118f2dee3afb16861c89a410aa69e6dc61
Shubhankar02 <shborade@gmail.com>
2018-11-19 18:45:21 +0530

    Add database url

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
+mongoose.connect(process.env.DATABASEURL || 'mongodb://localhost:27017/TodoApp', {
     useNewUrlParser: true
 });
 
